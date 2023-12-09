0c14fe981985abac038d53769b5736b1a9df9011
helljh <junho308917@naver.com>
2023-12-09 22:15:07 +0900

    create devB branch and update A&B

--- MyGit/src/practice/B.java
+++ MyGit/src/practice/B.java
@@ -3,6 +3,6 @@ package practice;
 public class B {
 	public void makeB(String name) {
 		System.out.println("이 클래스는 " + name + "이 만들었습니다");
-		System.out.println("devB 브랜치에서 수정했습니다");
+		System.out.println("devB에서 수정했습니다.");
 	}
 }
