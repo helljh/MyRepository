e65d5dd10e5b647956b2530a2ef016051a8c479b
helljh <junho308917@naver.com>
2023-12-09 22:11:13 +0900

    update in devB branch

--- MyGit/src/practice/A.java
+++ MyGit/src/practice/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("A 클래스입니다!");
+		System.out.println("devB 브랜치에서 수정했습니다");
 	}
 
 }
